ba39b434a46bd3128ce57bc7ee8b84e432140f4f
bugahontas <contatohelmaqui@gmail.com>
2022-05-19 20:50:10 -0300

    finished content-2

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
     $(".content-1").fadeOut(300);
     $(".content-2").fadeIn(800);
   })
-  
+  $(".btn-info").click(function() {
+    $(".content-2").fadeOut(300);
+    $(".content-1").fadeIn(800);
+  })
   
 })
