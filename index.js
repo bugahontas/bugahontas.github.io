6bfe212c1e378b8b3ceb3a856b4b6adbebff26c9
bugahontas <contatohelmaqui@gmail.com>
2022-05-21 15:11:20 -0300

    created contents 4 and 5

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 $(document).ready(function() {
   $(".card").fadeIn(800);
   $(".content-1").fadeIn(1500);
+  
   $(".btn-about").click(function() {
     $(".content-1").fadeOut(300);
     $(".content-2").fadeIn(800);
   })
-  $(".btn-why").click(function() {
+  
+  $(".btn-what").click(function() {
     $(".content-2").fadeOut(300);
     $(".content-3").fadeIn(800);
+  })
+  
+  $(".btn-fix").click(function() {
+    $(".content-3").fadeOut(300);
+    $(".content-4").fadeIn(800);
+  })
+
+  $(".btn-finally").click(function() {
+    $(".content-4").fadeOut(300);
+    $(".content-5").fadeIn(800);
+  })
+
+  $(".btn-back").click(function() {
+    $(".content-5").fadeOut(300);
+    $(".content-1").fadeIn(800);
   })
   
 })
